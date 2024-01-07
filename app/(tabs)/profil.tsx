b8d5b33eed591e1e6a20aee7fe7d03310df5be25
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-07 01:10:00 +0100

    💄 styles: update profile screen

--- app/(tabs)/profil.tsx
+++ app/(tabs)/profil.tsx
@@ -1,16 +1,36 @@
-import { Button, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, useColorScheme } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import { useRouter } from "expo-router";
+import Colors from "../../constants/Colors";
 
 export default function ProfilScreen() {
 	const router = useRouter();
+	const colorScheme = useColorScheme();
 	
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Profil</Text>
-			<Button title="Push" onPress={() => router.push("/hero")} />
+			<Pressable
+				onPress={() => router.push("/hero")}
+				style={({ pressed }) => [
+					styles.button,
+					{
+						opacity: pressed ? 0.5 : 1,
+						backgroundColor: Colors[colorScheme ?? "light"].tint,
+					},
+				]}
+			>
+				<Text
+					style={[
+						styles.textButton,
+						{ color: "black" },
+					]}
+				>
+					Go to Hero Screen
+				</Text>
+			</Pressable>
 			<View
 				style={styles.separator}
 				lightColor="#eee"
@@ -35,5 +55,23 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: "80%",
+	},
+	textButton: {
+		fontSize: 20,
+		fontWeight: "500",
+	},
+	button: {
+		width: "90%",
+		paddingVertical: 15,
+
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 10,
+
+		borderWidth: 1,
+		borderRadius: 50,
+		borderColor: "transparent",
 	},
 });
