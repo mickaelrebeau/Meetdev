d6dd5f13db9602e20129a6e83e9ea63ba8c45fc2
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-10 00:45:16 +0100

    🛠️ build: update messages data

--- constants/Messages.ts
+++ constants/Messages.ts
@@ -2,94 +2,146 @@ export const Messages = [
 	{
 		id: 1,
 		name: "L'équipe Meetdev",
+		imgUrl: require("../assets/images/meetdev.png"),
 		messages_received: [
 			{
 				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
 	{
 		id: 2,
 		name: "Mike_dreeman",
+		imgUrl: require("../assets/images/mike.png"),
 		messages_received: [
 			{
 				content: "Hello there!",
-				date: new Date("07/01/2024 10:45:00"),
+				date: new Date("2024-01-01T10:45:00"),
 			},
 			{
 				content: "How are you?",
-				date: new Date("07/01/2024 10:46:00"),
+				date: new Date("2024-01-01T10:46:00"),
 			},
 		],
 		messages_send: [
 			{
 				content: "Hi",
-				date: new Date("07/01/2024 10:50:00"),
+				date: new Date("2024-01-01T10:50:00"),
 			},
 			{
 				content: "I'm good and you?",
-				date: new Date("07/01/2024 10:51:00"),
+				date: new Date("2024-01-01T10:51:00"),
 			},
 		],
 	},
 	{
 		id: 3,
-		name: "L'équipe Meetdev",
+		name: "Homer Simpson",
+		imgUrl: require("../assets/images/homer.png"),
 		messages_received: [
 			{
-				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				content: "Trying is the first step towards failure.",
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
 	{
 		id: 4,
-		name: "L'équipe Meetdev",
+		name: "FBI",
+		imgUrl: require("../assets/images/fbi.png"),
 		messages_received: [
 			{
-				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				content: "We are looking for you bitch !",
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
 	{
 		id: 5,
-		name: "L'équipe Meetdev",
+		name: "Elon Musk",
+		imgUrl: require("../assets/images/elon.png"),
 		messages_received: [
 			{
-				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				content: "I would like to die on Mars. Just not on impact.",
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
 	{
 		id: 6,
-		name: "L'équipe Meetdev",
+		name: "Emmanuel Macron",
+		imgUrl: require("../assets/images/macron.png"),
 		messages_received: [
 			{
-				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				content: "We are at War !",
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
 	{
 		id: 7,
-		name: "L'équipe Meetdev",
+		name: "Mark Zuckerberg",
+		imgUrl: require("../assets/images/mark.png"),
 		messages_received: [
 			{
-				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				content:
+					"I wear the same outfit or, at least, a different copy of it almost every day.",
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
 	{
 		id: 8,
-		name: "L'équipe Meetdev",
+		name: "Naruto Uzumaki",
+		imgUrl: require("../assets/images/naruto.png"),
 		messages_received: [
 			{
-				content: "Hello there! Welcome to Meetdev !",
-				date: new Date("07/01/2024 10:45:00"),
+				content:
+					"A smile is the best way to get away with trouble even if it’s a fake one",
+				date: new Date("2024-01-01T10:45:00"),
+			},
+		],
+		messages_send: [
+			{
+				content: "Hi !",
+				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
