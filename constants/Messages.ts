1b3743409acb50adf42f5ec5edc36d9c92752261
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-10 04:46:56 +0100

    🛠️ build: update messages data

--- constants/Messages.ts
+++ constants/Messages.ts
@@ -3,16 +3,11 @@ export const Messages = [
 		id: 1,
 		name: "L'équipe Meetdev",
 		imgUrl: require("../assets/images/meetdev.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "L'équipe Meetdev",
 				content: "Hello there! Welcome to Meetdev !",
 				date: new Date("2024-01-01T10:45:00"),
-			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
@@ -20,22 +15,24 @@ export const Messages = [
 		id: 2,
 		name: "Mike_dreeman",
 		imgUrl: require("../assets/images/mike.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "Mike_dreeman",
 				content: "Hello there!",
 				date: new Date("2024-01-01T10:45:00"),
 			},
 			{
+				sender: "Mike_dreeman",
 				content: "How are you?",
 				date: new Date("2024-01-01T10:46:00"),
 			},
-		],
-		messages_send: [
 			{
+				sender: "me",
 				content: "Hi",
 				date: new Date("2024-01-01T10:50:00"),
 			},
 			{
+				sender: "me",
 				content: "I'm good and you?",
 				date: new Date("2024-01-01T10:51:00"),
 			},
@@ -45,16 +42,11 @@ export const Messages = [
 		id: 3,
 		name: "Homer Simpson",
 		imgUrl: require("../assets/images/homer.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "Homer Simpson",
 				content: "Trying is the first step towards failure.",
 				date: new Date("2024-01-01T10:45:00"),
-			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
@@ -62,68 +54,48 @@ export const Messages = [
 		id: 4,
 		name: "FBI",
 		imgUrl: require("../assets/images/fbi.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "FBI",
 				content: "We are looking for you bitch !",
 				date: new Date("2024-01-01T10:45:00"),
 			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
-			},
-		],
+		]
 	},
 	{
 		id: 5,
 		name: "Elon Musk",
 		imgUrl: require("../assets/images/elon.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "Elon Musk",
 				content: "I would like to die on Mars. Just not on impact.",
 				date: new Date("2024-01-01T10:45:00"),
 			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
-			},
-		],
+		]
 	},
 	{
 		id: 6,
 		name: "Emmanuel Macron",
 		imgUrl: require("../assets/images/macron.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "Emmanuel Macron",
 				content: "We are at War !",
 				date: new Date("2024-01-01T10:45:00"),
 			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
-			},
-		],
+		]
 	},
 	{
 		id: 7,
 		name: "Mark Zuckerberg",
 		imgUrl: require("../assets/images/mark.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "Mark Zuckerberg",
 				content:
 					"I wear the same outfit or, at least, a different copy of it almost every day.",
 				date: new Date("2024-01-01T10:45:00"),
-			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
 			},
 		],
 	},
@@ -131,18 +103,13 @@ export const Messages = [
 		id: 8,
 		name: "Naruto Uzumaki",
 		imgUrl: require("../assets/images/naruto.png"),
-		messages_received: [
+		chat: [
 			{
+				sender: "Naruto Uzumaki",
 				content:
 					"A smile is the best way to get away with trouble even if it’s a fake one",
 				date: new Date("2024-01-01T10:45:00"),
 			},
-		],
-		messages_send: [
-			{
-				content: "Hi !",
-				date: new Date("2024-01-01T10:44:00"),
-			},
-		],
+		]
 	},
 ];
