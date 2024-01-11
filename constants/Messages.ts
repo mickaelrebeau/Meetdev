15e9eb9194ba701086760947fa8b766007547671
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-11 08:50:24 +0100

    🛠️ build: update messages

--- constants/Messages.ts
+++ constants/Messages.ts
@@ -36,6 +36,46 @@ export const Messages = [
 				content: "I'm good and you?",
 				date: new Date("2024-01-01T10:51:00"),
 			},
+			{
+				sender: "Mike_dreeman",
+				content: "I'm good too",
+				date: new Date("2024-01-05T20:12:00"),
+			},
+			{
+				sender: "me",
+				content: "Nice. How old are you?",
+				date: new Date("2024-01-05T20:12:00"),
+			},
+			{
+				sender: "Mike_dreeman",
+				content: "I'm 29 and you ?",
+				date: new Date("2024-01-05T20:13:00"),
+			},
+			{
+				sender: "me",
+				content: "I'm 24 😉",
+				date: new Date("2024-01-05T20:14:00"),
+			},
+			{
+				sender: "Mike_dreeman",
+				content: "Okay. What do you do for a living ?",
+				date: new Date("2024-01-05T20:15:00"),
+			},
+			{
+				sender: "me",
+				content: "I work as a developer and you ? 💻",
+				date: new Date("2024-01-05T20:16:00"),
+			},
+			{
+				sender: "Mike_dreeman",
+				content: "I'm a photographer 📸",
+				date: new Date("2024-01-05T20:17:00"),
+			},
+			{
+				sender: "me",
+				content: "Very nice 👍",
+				date: new Date("2024-01-05T20:18:00"),
+			}
 		],
 	},
 	{
