261dd8926fe37fde54fbde853849cc9861dd64a2
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-12 02:50:55 +0100

    🛠️ build: add scroll bottom component

---
+++ components/ScrollBottom.tsx
@@ -0,0 +1,7 @@
+import { FontAwesome } from "@expo/vector-icons";
+
+export const ScrollBottom = () => {
+    return (
+        <FontAwesome name="angle-double-down" size={24} color="black" />
+    );
+};
