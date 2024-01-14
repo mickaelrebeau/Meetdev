2cec3153d813d56b620517318e01824ced97933c
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-14 03:50:16 +0100

    🛠️ build: update notifications page (temporaire)

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -1,13 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet } from "react-native";
+import { Platform, Pressable, StyleSheet } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
+import { useState } from "react";
+import { NewMatch } from "../components/NewMatchModal";
 
 export default function NotificationsScreen() {
+	const [modalVisible, setModalVisible] = useState(false);
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Notifications</Text>
+			<Pressable
+				onPress={() => setModalVisible(!modalVisible)}
+				style={({ pressed }) => [styles.button, { opacity: pressed ? 0.5 : 1 }]}
+			>
+				<Text style={styles.title}>Go to new match screen</Text>
+			</Pressable>
+			<NewMatch modalVisible={modalVisible} setModalVisible={setModalVisible} />
 			<View
 				style={styles.separator}
 				lightColor="#eee"
@@ -26,6 +36,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: "center",
 		justifyContent: "center",
+		gap: 20,
 	},
 	title: {
 		fontSize: 20,
@@ -35,5 +46,20 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: "80%",
+	},
+	button: {
+		width: "90%",
+		paddingVertical: 15,
+
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 10,
+
+		backgroundColor: "transparent",
+		borderRadius: 50,
+		borderColor: "#2f95dc",
+		borderWidth: 2,
 	},
 });
