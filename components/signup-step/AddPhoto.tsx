f1cfd031bf45266ab3c1005c229d7e70448a59bd
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-18 16:43:38 +0100

    💄 styles: update add photo component

--- components/signup-step/AddPhoto.tsx
+++ components/signup-step/AddPhoto.tsx
@@ -1,16 +1,37 @@
-import { Pressable, StyleSheet } from "react-native";
+import { Image, Pressable, StyleSheet } from "react-native";
 import { Text, View } from "../Themed";
 import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 export const AddPhoto = () => {
+	const [image, setImage] = useState<string | null>(null);
+
+	const pickImage = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setImage(result.assets[0].uri);
+		}
+	};
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Add a photo of yourself or an avatar</Text>
 			<View>
 				<View style={styles.media}>
-					<View style={styles.photo} />
+					{image !== null ? (
+						<Image source={{ uri: image }} style={styles.image} />
+					) : (
+						<View style={styles.photo} />
+					)}
 				</View>
 				<Pressable
+					onPress={pickImage}
 					style={({ pressed }) => [
 						styles.add,
 						{
@@ -26,30 +47,36 @@ export const AddPhoto = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+	container: {
+		flex: 1,
 		alignItems: "center",
 		gap: 30,
-    },
+	},
 	media: {
 		padding: 10,
 		width: "100%",
 		display: "flex",
 		alignItems: "flex-start",
 	},
+	image: {
+		width: 200,
+		height: 200,
+		borderWidth: 2,
+		borderRadius: 10,
+	},
 	photo: {
-		width: 120,
-		height: 120,
+		width: 200,
+		height: 200,
 		borderWidth: 2,
 		borderRadius: 10,
 		borderColor: "gray",
 		borderStyle: "dashed",
 		backgroundColor: "#3e3e3e",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "500",
-    },
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "500",
+	},
 	form: {
 		marginVertical: 20,
 		width: "90%",
