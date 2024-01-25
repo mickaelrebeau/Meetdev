9a2a706dc76e29b9c3a7559cfbcd439664443af0
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-25 20:50:12 +0100

    🛠️ build: update user type

--- @types/Users.d.ts
+++ @types/Users.d.ts
@@ -1,9 +1,11 @@
 type User = {
 	id: number;
-	name: string;
-	job: string;
-	company: string;
-	image: ImageSourcePropType;
+	image_uri: string;
 	bio: string;
+	username: string;
+	company: string;
+	post: string;
+	github_url: string;
+	portfolio_url: string;
 	tags: string[];
 };
