e135e8c9f4538989cef50c6caf02d6b303781b33
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-02-24 13:31:29 +0100

    🛠️ build: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for expo-router
-      'expo-router/babel',
-      // Other plugins
-      'react-native-reanimated/plugin',
-    ],
-  };
+	api.cache(true);
+	return {
+    presets: ["babel-preset-expo"],
+		plugins: [
+			// Required for expo-router
+			"expo-router/babel",
+			// Other plugins
+			"react-native-reanimated/plugin",
+		],
+	};
 };
