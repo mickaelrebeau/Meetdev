d946cf93e5317d5e3df191a170ab871cd8f4fb17
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-02-24 13:34:26 +0100

    🛠️ build: update app layout screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,10 @@ import {
 	ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack, router } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
-import * as Updates from "expo-updates";
-import { FirebaseAuthTypes } from "@react-native-firebase/auth";
-import auth from "@react-native-firebase/auth";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -51,65 +49,45 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
 	const colorScheme = useColorScheme();
-
-	const checkUpdate = async () => {
-		try {
-			const update = await Updates.checkForUpdateAsync();
-
-			if (update.isAvailable) {
-				await Updates.fetchUpdateAsync();
-				await Updates.reloadAsync();
-			}
-		} catch (error) {
-			alert(`Error checking for updates: ${error}`);
-		}
-	};
-	
-	const onAuthStateChange = (user: FirebaseAuthTypes.User | null) => {
-		if (user) {
-			router.push("/home");
-		} else {
-			router.push("/");
-		}
-	};
-
-	useEffect(() => {
-		checkUpdate();
-		auth().onAuthStateChanged(onAuthStateChange);
-	}, []);
+	const { authState } = useAuth();
 
 	return (
-		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<Stack>
-				<Stack.Screen name="index" options={{ headerShown: false }} />
-				<Stack.Screen name="signup-step" options={{ headerShown: false }} />
-				<Stack.Screen
-					name="forgot-password"
-					options={{ presentation: "modal", title: "Reset Password" }}
-				/>
-				<Stack.Screen
-					name="auth"
-					options={{ headerShown: false, presentation: "modal" }}
-				/>
-				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				<Stack.Screen
-					name="chat/[id]"
-					options={{ presentation: "modal", headerShown: false }}
-				/>
-				<Stack.Screen
-					name="notifications"
-					options={{ presentation: "modal", title: "Notifications" }}
-				/>
-				<Stack.Screen
-					name="settings"
-					options={{ presentation: "modal", title: "Settings" }}
-				/>
-				<Stack.Screen
-					name="edit-profile"
-					options={{ presentation: "modal", title: "Edit Profile" }}
-				/>
-				<Stack.Screen name="card" options={{ headerShown: false }} />
-			</Stack>
-		</ThemeProvider>
+		<AuthProvider>
+			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+				<Stack>
+					{authState?.authenticated ? (
+						<Stack.Screen name="index" options={{ headerShown: false }} />
+					) : (
+						<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+					)}
+					<Stack.Screen name="signup-step" options={{ headerShown: false }} />
+					<Stack.Screen
+						name="forgot-password"
+						options={{ presentation: "modal", title: "Reset Password" }}
+					/>
+					<Stack.Screen
+						name="auth"
+						options={{ headerShown: false, presentation: "modal" }}
+					/>
+					<Stack.Screen
+						name="chat/[id]"
+						options={{ presentation: "modal", headerShown: false }}
+					/>
+					<Stack.Screen
+						name="notifications"
+						options={{ presentation: "modal", title: "Notifications" }}
+					/>
+					<Stack.Screen
+						name="settings"
+						options={{ presentation: "modal", title: "Settings" }}
+					/>
+					<Stack.Screen
+						name="edit-profile"
+						options={{ presentation: "modal", title: "Edit Profile" }}
+					/>
+					<Stack.Screen name="card" options={{ headerShown: false }} />
+				</Stack>
+			</ThemeProvider>
+		</AuthProvider>
 	);
 }
