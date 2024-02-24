2b0821e133a391a3ceb1a3781d19fa3ba2b7a5b5
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-02-24 13:34:17 +0100

    🛠️ build: update hero screen

--- app/index.tsx
+++ app/index.tsx
@@ -5,74 +5,27 @@ import { Text, View } from "../components/Themed";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { LogoSvg } from "../assets/images/LogoSvg";
 import { useRouter } from "expo-router";
-import auth from "@react-native-firebase/auth";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect } from "react";
-import * as WebBrowser from "expo-web-browser";
-import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
-import { createTokenWithCode } from "../utils/createToken";
-
-WebBrowser.maybeCompleteAuthSession();
-
-const discovery = {
-	authorizationEndpoint: "https://github.com/login/oauth/authorize",
-	tokenEndpoint: "https://github.com/login/oauth/access_token",
-	revocationEndpoint:
-		"https://github.com/settings/connections/applications/1d346e1b8f7973c3cb2d",
-};
 
 export default function HeroScreen() {
 	const router = useRouter();
 
-	const [request, response, promptAsync] = useAuthRequest(
-		{
-			clientId: "1d346e1b8f7973c3cb2d",
-			scopes: [],
-			redirectUri: makeRedirectUri(),
-		},
-		discovery
-	);
-
-	const handleResponse = async () => {
-		if (response?.type === "success") {
-			const { code } = response.params;
-			const { token_type, scope, access_token } = await createTokenWithCode(
-				code
-			);
-
-			if (!access_token) {
-				alert("Something went wrong. Please try again.");
-				return;
-			}
-
-			const credential = auth.GithubAuthProvider.credential(access_token);
-			await auth().signInWithCredential(credential);
-
-			router.push("/signup-step");
-		}
-	};
-
-	const handleGoogleSignin = async () => {
-		try {
-			await GoogleSignin.hasPlayServices();
-			const { idToken } = await GoogleSignin.signIn();
-			const credential = auth.GoogleAuthProvider.credential(idToken);
-			await auth().signInWithCredential(credential);
-
-			router.push("/signup-step");
-		} catch (error) {
-			alert(error);
-		}
-	};
-
 	useEffect(() => {
-		GoogleSignin.configure({
-			webClientId:
-				"561315881893-pqn4j9i9l2gv8s8no2ckgj9fe2k0obro.apps.googleusercontent.com",
-		});
+		const checkUserId = async () => {
+			try {
+				const userId = await AsyncStorage.getItem("@userId");
 
-		handleResponse();
-	}, [response]);
+				if (userId) {
+					router.push("/home");
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		checkUserId();
+	}, []);
 
 	return (
 		<ImageBackground
@@ -83,32 +36,18 @@ export default function HeroScreen() {
 				<LogoSvg />
 				<Text style={[styles.titleLogo, { color: "white" }]}>Meetdev</Text>
 			</View>
-			<Text style={[styles.title, { color: "white" }]}>
+			<Text style={[styles.title, { color: "white", marginBottom: 10 }]}>
 				Everything starts with code.
 			</Text>
+			<Text
+				style={[
+					styles.title,
+					{ color: "white", fontWeight: "400", marginBottom: 10 },
+				]}
+			>
+				Let's build something together.
+			</Text>
 			<View style={styles.authButton}>
-				<Pressable
-					onPress={() => {
-						promptAsync();
-					}}
-					style={({ pressed }) => [
-						styles.button,
-						{ opacity: pressed ? 0.5 : 1 },
-					]}
-				>
-					<FontAwesome name="github" size={25} color="white" />
-					<Text style={styles.textButton}>Sign Up with Github</Text>
-				</Pressable>
-				<Pressable
-					onPress={handleGoogleSignin}
-					style={({ pressed }) => [
-						styles.button,
-						{ opacity: pressed ? 0.5 : 1 },
-					]}
-				>
-					<FontAwesome name="google" size={25} color="white" />
-					<Text style={styles.textButton}>Sign Up with Google</Text>
-				</Pressable>
 				<Pressable
 					onPress={() => router.push("/auth")}
 					style={({ pressed }) => [
@@ -158,6 +97,7 @@ const styles = StyleSheet.create({
 		color: "#fff",
 	},
 	authButton: {
+		height: 220,
 		width: "100%",
 		display: "flex",
 		flexDirection: "column",
