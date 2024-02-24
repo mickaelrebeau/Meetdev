f20edd6a03659928952d25f8c7f1ea647d509728
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-02-24 13:31:53 +0100

    🛠️ build: update users data

--- constants/Users.ts
+++ constants/Users.ts
@@ -4,8 +4,7 @@ export const Users = [
 		username: "Mike_dreeman",
 		post: "Frontend Developer",
 		company: "Apple",
-		image_uri:
-			"file:///data/user/0/com.dreeman.meetdev/cache/ImagePicker/cd9ed1cb-9009-470d-8ba2-78c22b0ccafd.jpeg",
+		image: require("../assets/images/mike.png"),
 		bio: "Lorem ipsum dolor sit amet consectetur. Imperdiet urna proin et leo sollicitudin facilisi dolor magna. Augue tristique amet faucibus dictumenim viverra. Ullamcorper risus felis magna sem risus vestibulum miaugue.",
 		github_url: "https://github.com/Mike-dreeman",
 		portfolio_url: "https://www.mike-dreeman-portfolio.vercel.app",
@@ -16,8 +15,7 @@ export const Users = [
 		username: "Mickael Rébeau",
 		post: "Backend Developer",
 		company: "Google",
-		image_uri:
-			"file:///data/user/0/com.dreeman.meetdev/cache/ImagePicker/cd9ed1cb-9009-470d-8ba2-78c22b0ccafd.jpeg",
+		image: require("../assets/images/mike.png"),
 		bio: "Lorem ipsum dolor sit amet consectetur. Imperdiet urna proin et leo sollicitudin facilisi dolor magna. Augue tristique amet faucibus dictumenim viverra. Ullamcorper risus felis magna sem risus vestibulum miaugue.",
 		github_url: "https://github.com/Mike-dreeman",
 		portfolio_url: "https://www.mike-dreeman-portfolio.vercel.app",
@@ -28,8 +26,7 @@ export const Users = [
 		username: "Mike Dreeman",
 		post: "Fullstack Developer",
 		company: "Facebook",
-		image_uri:
-			"file:///data/user/0/com.dreeman.meetdev/cache/ImagePicker/cd9ed1cb-9009-470d-8ba2-78c22b0ccafd.jpeg",
+		image: require("../assets/images/mike.png"),
 		bio: "Lorem ipsum dolor sit amet consectetur. Imperdiet urna proin et leo sollicitudin facilisi dolor magna. Augue tristique amet faucibus dictumenim viverra. Ullamcorper risus felis magna sem risus vestibulum miaugue.",
 		github_url: "https://github.com/Mike-dreeman",
 		portfolio_url: "https://www.mike-dreeman-portfolio.vercel.app",
